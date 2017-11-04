4739cdea69c0b91f12e72827da6ece0f8f8d3f29
Catalin Saveanu <catalinsaveanu@gmail.com>
2017-11-04 17:30:22 +0200

    moved game logic from react component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,95 +1,27 @@
 import React, { Component } from 'react';
 import {Layer, Line, Circle, Stage, Group, Text} from 'react-konva';
+import GameLogic from '../game/GameLogic';
 
-import {MAIN_COLOR, GATE_W, FIELD_W, FIELD_H, BORDER_POINTS, SPACE_BETWEEN_POINTS, STROKE_W, POINTS_W, POINTS_H, MOVES_COLOR, INF, TOTAL_POINTS, P_WEIGHT} from 'utils/constants';
+import {MAIN_COLOR, GATE_W, FIELD_W, FIELD_H, BORDER_POINTS, SPACE_BETWEEN_POINTS, STROKE_W, POINTS_W , POINTS_H, MOVES_COLOR} from 'utils/constants';
 
 class Game extends Component {
-    state = {
-        fieldMoves: [],
-        fieldPoints: new Array(99).fill(MAIN_COLOR),
-        winner: ''
-    }
+    constructor(props) {
+        super(props);
 
-    edgeMatrix = [];
-    vertexMatrix = [];
-    turn = 0;
-    cVertex = (TOTAL_POINTS - 1) / 2;
+        this.gameLogic = new GameLogic(POINTS_W, POINTS_H);
 
-    initMoves() {
-        let i = 0,
-            j = 0;
-
-        for (i = 0; i < POINTS_H; i++) {
-            this.vertexMatrix[i] = new Array(POINTS_W);
-            for (j = 0; j < POINTS_W; j++) {
-                this.vertexMatrix[i][j] = i * POINTS_W + j;
-            }
-        }
-
-        console.table(this.vertexMatrix);
-
-        for (i = 0; i < TOTAL_POINTS; i++) {
-            this.edgeMatrix[i] = new Array(TOTAL_POINTS).fill(INF);
-            this.edgeMatrix[i][i] = 0;
-        }
-
-        for (i = 0; i < POINTS_H; i++) {
-            for (j = 0; j < POINTS_W; j++) {
-                let vertexA = this.vertexMatrix[i][j],
-                    isBorderA = this.isBorder(vertexA);
-
-                this.getVertexNeighbors(vertexA).forEach(vertexB => {
-                    let isBorderB = this.isBorder(vertexB);
-
-                    if (isBorderA && !isBorderB) {
-                        this.edgeMatrix[vertexA][vertexB] = 1;
-                    } else if (!isBorderA && isBorderB) {
-                        this.edgeMatrix[vertexA][vertexB] = 0;
-                    } else if (!isBorderA && !isBorderB) {
-                        this.edgeMatrix[vertexA][vertexB] = 1;
-                    } else if (isBorderA && isBorderB && Math.abs(vertexA - vertexB) >= 10) {
-                        this.edgeMatrix[vertexA][vertexB] = 0;
-                    }
-                });
-            }
-        };
-
-        console.table(this.edgeMatrix);
-    }
-
-    getVertexIndices(vertex) {
-        return {
-            i: Math.floor(vertex / POINTS_W),
-            j: vertex % POINTS_W
+        this.state = {
+            fieldMoves: this.getMoves(),
+            fieldPoints: new Array(99).fill(MAIN_COLOR),
+            winner: ''
         }
     }
 
-    getVertexNeighbors(vertex) {
-        let neighbors = [],
-            vertexIndices = this.getVertexIndices(vertex);
-
-        for (let angle = 0; angle < 2 * Math.PI; angle += Math.PI/4) {
-            let xSign = Math.sign(Math.cos(angle) + 10 - 10),
-                ySign = Math.sign(Math.sin(angle) + 10 - 10);
-
-            if (vertexIndices.i + xSign > -1 && vertexIndices.i + xSign < POINTS_H && vertexIndices.j + ySign > -1 && vertexIndices.j + ySign < POINTS_W) {
-                neighbors.push(this.vertexMatrix[vertexIndices.i + xSign][vertexIndices.j + ySign]);
-            }
-        }
-
-        return neighbors;
-    }
-
-    getAvailableVertexNeighbors(vertex) {
-        this.getVertexNeighbors(vertex).filter(vertexNeighbor => {
-            return (this.edgeMatrix[vertex][vertexNeighbor] === 0 || this.edgeMatrix[vertex][vertexNeighbor] === 1)
-        })
-    }
 
     getPointPosition(point) {
         return {
-            x: point % POINTS_W,
-            y: Math.floor(point / POINTS_W)
+            x: point % this.gameLogic.width,
+            y: Math.floor(point / this.gameLogic.width)
         }
     }
 
@@ -105,36 +37,21 @@ class Game extends Component {
     getMoves() {
         var moves = [];
 
-        for (let i = 0; i < TOTAL_POINTS; i++) {
-            for (let j = 0; j < TOTAL_POINTS; j++) {
-                let d = this.edgeMatrix[i][j];
-
-                if (d === P_WEIGHT || d === 2 * P_WEIGHT) {
-                    let playerIndex = d / P_WEIGHT - 1,
-                        linePoints = this.getPoints(i, j);
+        for (let i = 0; i < this.gameLogic.totalVertices; i++) {
+            for (let j = 0; j < this.gameLogic.totalVertices; j++) {
+                if (this.gameLogic.isPlayerMove(i, j)) {
+                    let linePoints = this.getPoints(i, j),
+                        color = MOVES_COLOR[this.gameLogic.getPlayerFromMove(i, j)];
 
                     //this.changePointsColor(i, MOVES_COLOR[playerIndex]);
                     //this.changePointsColor(j, MOVES_COLOR[playerIndex]);
         
-                    moves.push(<Line key={`move-${i}_${j}`} points={linePoints} stroke={MOVES_COLOR[playerIndex]} strokeWidth={STROKE_W}/>);
+                    moves.push(<Line key={`move-${i}_${j}`} points={linePoints} stroke={color} strokeWidth={STROKE_W}/>);
                 }
             }
         }
 
         return moves;
-    }
-
-    isBorder(vertex) {
-        let indices = this.getVertexIndices(vertex);
-        
-        return (indices.i === 0 || indices.j === 0);
-    }
-
-    componentDidMount() {
-        this.initMoves();
-        this.setState({
-            fieldMoves: this.getMoves()
-        });
     }
 
     changePointsColor(pointIndex, color) {
@@ -148,11 +65,11 @@ class Game extends Component {
     }
 
     getFieldPoints() {
-        let points = new Array(TOTAL_POINTS).fill(1);
+        let points = new Array(this.gameLogic.totalVertices).fill(1);
 
         return points.map((value, index) => {
-            const xpos = (index % POINTS_W) * SPACE_BETWEEN_POINTS,
-                  ypos = Math.floor(index / POINTS_W) * SPACE_BETWEEN_POINTS;
+            const xpos = (index % this.gameLogic.width) * SPACE_BETWEEN_POINTS,
+                  ypos = Math.floor(index / this.gameLogic.width) * SPACE_BETWEEN_POINTS;
 
             return (
                 <Group key={`group-${index}`}>
@@ -163,80 +80,11 @@ class Game extends Component {
         })
     }
 
-    makeMove(fromVertex, toVertex) {
-        let initValue = this.edgeMatrix[fromVertex][toVertex];
-
-        this.edgeMatrix[fromVertex][toVertex] = this.edgeMatrix[toVertex][fromVertex] = (this.turn + 1) * P_WEIGHT;
-        this.updateNeighbors(toVertex);
-
-        this.setState({
-            fieldMoves: this.getMoves()
-        });
-
-        if (initValue !== 0) {
-            this.changeTurn();
-        }
-
-        this.cVertex = toVertex;
-
-        this.checkIfGameIsOver(toVertex);
-
-        this.logNeighbors(toVertex);
-        console.table(this.edgeMatrix);
-    }
-
-    checkIfGameIsOver(vertex) {
-        let isGameOver = (vertex === 44 || vertex === 54),
-            winner = this.turn + 1;
-
-        if (!isGameOver) {
-            let weHaveMoves = this.getVertexNeighbors(vertex).some(vertexNeighbor => {
-                return this.weCanMove(vertex, vertexNeighbor);
-            });
-
-            if (!weHaveMoves) {
-                isGameOver = true;
-                winner = (this.turn + 1) % 2;
-            }   
-            
-        }
-
-        if (isGameOver) {
-            this.setState({
-                winner: `Player ${winner + 1} Wins!`
-            })
-        }
-    }
-
-    updateNeighbors(vertex) {
-        this.getVertexNeighbors(vertex).forEach(vertexNeighbor => {
-            let isBorderNeighbor = this.isBorder(vertexNeighbor);
-
-            if (!isBorderNeighbor && this.edgeMatrix[vertex][vertexNeighbor] === 1 && this.wasVisited(vertexNeighbor)) {
-                this.edgeMatrix[vertex][vertexNeighbor] = this.edgeMatrix[vertexNeighbor][vertexNeighbor] = 0;
-            }
-        });
-    }
-
-    wasVisited(vertex) {
-        return this.getVertexNeighbors(vertex).some(vertexNeighbor => {
-            return this.edgeMatrix[vertex][vertexNeighbor] === P_WEIGHT || this.edgeMatrix[vertex][vertexNeighbor] === 2 * P_WEIGHT;
-        });
-    }
-
-    changeTurn() {
-        this.turn = (this.turn + 1) % 2;
-    }
-
-    weCanMove(fromVertex, toVertex) {
-        return (fromVertex !== toVertex && (this.edgeMatrix[fromVertex][toVertex] === 0 || this.edgeMatrix[fromVertex][toVertex] === 1));
-    }
-
     onOverPoints(e) {
         let circle = e.target,
             vertex = parseInt(circle.id().substr(6), 10);
         
-        if (this.weCanMove(this.cVertex, vertex)) {
+        if (this.gameLogic.weCanMoveTo(vertex)) {
             circle.to({scaleX: 1.5, scaleY: 1.5, duration: 0.1});
         }
     }
@@ -250,18 +98,19 @@ class Game extends Component {
         let circle = e.target,
             vertex = parseInt(circle.id().substr(6), 10);
 
-        if (this.weCanMove(this.cVertex, vertex)) {
-            this.makeMove(this.cVertex, vertex);
+        if (this.gameLogic.weCanMoveTo(vertex)) {
+            this.gameLogic.makeMoveTo(vertex);
+
+            this.setState({
+                fieldMoves: this.getMoves()
+            });
+
+            if (this.gameLogic.isGameOver) {
+                this.setState({
+                    winner: `Player ${this.gameLogic.winner} Wins!`
+                });
+            };
         }
-    }
-
-    logNeighbors(vertex) {
-        let sortedNeighbors = this.getVertexNeighbors(vertex).sort((a, b) => a-b).map(vertexNeighbor => {
-            return `${vertexNeighbor}->${this.edgeMatrix[vertex][vertexNeighbor]}`;
-        });
-
-        console.log('Vertex:', vertex);
-        console.log('Neighbors:' + sortedNeighbors.join(','));
     }
 
     render() {        
---
+++ src/game/GameLogic.js
@@ -0,0 +1,222 @@
+const INF = Infinity,
+      P_WEIGHT = 10000;
+
+class GameLogic {    
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+        this._totalVertices = width * height;
+        this._edgeMatrix = [];
+        this._vertexMatrix = [];
+        this._playerTurn = 0;
+        this._cVertex = (this._totalVertices - 1) / 2;
+        this._isGameOver = false;
+        this._winner = '';
+
+        this.initMatrixes();
+    }
+
+    static get P_WEIGHT() {
+        return P_WEIGHT;
+    }
+
+    static get INF() {
+        return INF;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+    
+    get totalVertices() {
+        return this._totalVertices;
+    }
+
+    get playerTurn() {
+        return this._playerTurn;
+    }
+
+    get winner() {
+        return this._winner;
+    }
+
+    get isGameOver() {
+        return this._isGameOver;
+    }
+
+    
+    initMatrixes() {
+        let i = 0,
+            j = 0;
+
+        for (i = 0; i < this.height; i++) {
+            this._vertexMatrix[i] = new Array(this.width);
+            for (j = 0; j < this.width; j++) {
+                this._vertexMatrix[i][j] = i * this.width + j;
+            }
+        }
+
+        console.table(this._vertexMatrix);
+
+        for (i = 0; i < this.totalVertices; i++) {
+            this._edgeMatrix[i] = new Array(this.totalVertices).fill(INF);
+            this._edgeMatrix[i][i] = 0;
+        }
+
+        for (i = 0; i < this.height; i++) {
+            for (j = 0; j < this.width; j++) {
+                let vertexA = this._vertexMatrix[i][j],
+                    isBorderA = this.isBorder(vertexA);
+
+                this.getVertexNeighbors(vertexA).forEach(vertexB => {
+                    let isBorderB = this.isBorder(vertexB);
+
+                    if (isBorderA && !isBorderB) {
+                        this._edgeMatrix[vertexA][vertexB] = 1;
+                    } else if (!isBorderA && isBorderB) {
+                        this._edgeMatrix[vertexA][vertexB] = 0;
+                    } else if (!isBorderA && !isBorderB) {
+                        this._edgeMatrix[vertexA][vertexB] = 1;
+                    } else if (isBorderA && isBorderB && Math.abs(vertexA - vertexB) >= 10) {
+                        this._edgeMatrix[vertexA][vertexB] = 0;
+                    }
+                });
+            }
+        };
+
+        console.table(this._edgeMatrix);
+    }
+
+    isPlayerMove(i, j) {
+        const d = this._edgeMatrix[i][j];
+
+        return d === P_WEIGHT || d === 2 * P_WEIGHT;
+    }
+
+    isBorder(vertex) {
+        let indices = this.getVertexIndices(vertex);
+
+        return (indices.i === 0 || indices.j === 0);
+    }
+
+    getPlayerFromMove(i, j) {
+        const d = this._edgeMatrix[i][j];
+
+        return d / P_WEIGHT - 1;
+    }
+
+    getVertexIndices(vertex) {
+        return {
+            i: Math.floor(vertex / this.width),
+            j: vertex % this.width
+        }
+    }
+
+    getVertexNeighbors(vertex) {
+        let neighbors = [],
+            vertexIndices = this.getVertexIndices(vertex);
+
+        for (let angle = 0; angle < 2 * Math.PI; angle += Math.PI / 4) {
+            let xSign = Math.sign(Math.cos(angle) + 10 - 10),
+                ySign = Math.sign(Math.sin(angle) + 10 - 10);
+
+            if (vertexIndices.i + xSign > -1 && vertexIndices.i + xSign < this.height && vertexIndices.j + ySign > -1 && vertexIndices.j + ySign < this.width) {
+                neighbors.push(this._vertexMatrix[vertexIndices.i + xSign][vertexIndices.j + ySign]);
+            }
+        }
+
+        return neighbors;
+    }
+
+    getAvailableVertexNeighbors(vertex) {
+        this.getVertexNeighbors(vertex).filter(vertexNeighbor => {
+            return (this._edgeMatrix[vertex][vertexNeighbor] === 0 || this._edgeMatrix[vertex][vertexNeighbor] === 1)
+        })
+    }
+
+    makeMoveTo(toVertex) {
+        this.makeMove(this._cVertex, toVertex);
+    }
+
+    makeMove(fromVertex, toVertex) {
+        let initValue = this._edgeMatrix[fromVertex][toVertex];
+
+        this._edgeMatrix[fromVertex][toVertex] = this._edgeMatrix[toVertex][fromVertex] = (this.playerTurn + 1) * P_WEIGHT;
+        this.updateNeighbors(toVertex);
+        this.checkIfGameIsOver(toVertex);
+
+        if (initValue !== 0) {
+            this.changeTurn();
+        }
+
+        this._cVertex = toVertex;
+
+        this.logNeighbors(toVertex);
+    }
+
+    checkIfGameIsOver(vertex) {
+        let isGameOver = (vertex === 44 || vertex === 54);
+
+        this._winner = this.playerTurn + 1;
+
+        if (!isGameOver) {
+            let weHaveMoves = this.getVertexNeighbors(vertex).some(vertexNeighbor => {
+                return this.weCanMove(vertex, vertexNeighbor);
+            });
+
+            if (!weHaveMoves) {
+                isGameOver = true;
+                this._winner = (this.playerTurn + 1) % 2;
+            }
+        }
+
+        if (!isGameOver) {
+            this._winner = '';
+        }
+
+        this._isGameOver = isGameOver;
+    }
+
+    updateNeighbors(vertex) {
+        this.getVertexNeighbors(vertex).forEach(vertexNeighbor => {
+            let isBorderNeighbor = this.isBorder(vertexNeighbor);
+
+            if (!isBorderNeighbor && this._edgeMatrix[vertex][vertexNeighbor] === 1 && this.wasVisited(vertexNeighbor)) {
+                this._edgeMatrix[vertex][vertexNeighbor] = this._edgeMatrix[vertexNeighbor][vertexNeighbor] = 0;
+            }
+        });
+    }
+
+    wasVisited(vertex) {
+        return this.getVertexNeighbors(vertex).some(vertexNeighbor => {
+            return this._edgeMatrix[vertex][vertexNeighbor] === P_WEIGHT || this._edgeMatrix[vertex][vertexNeighbor] === 2 * P_WEIGHT;
+        });
+    }
+
+    changeTurn() {
+        this._playerTurn = (this._playerTurn + 1) % 2;
+    }
+
+    weCanMove(fromVertex, toVertex) {
+        return (fromVertex !== toVertex && (this._edgeMatrix[fromVertex][toVertex] === 0 || this._edgeMatrix[fromVertex][toVertex] === 1));
+    }
+
+    weCanMoveTo(toVertex) {
+        return this.weCanMove(this._cVertex, toVertex);
+    }
+
+    logNeighbors(vertex) {
+        let sortedNeighbors = this.getVertexNeighbors(vertex).sort((a, b) => a - b).map(vertexNeighbor => {
+            return `${vertexNeighbor}->${this._edgeMatrix[vertex][vertexNeighbor]}`;
+        });
+
+        console.log('Vertex:', vertex);
+        console.log('Neighbors:' + sortedNeighbors.join(','));
+    }
+}
+
+export default GameLogic;
