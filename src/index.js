2effe174c5dc60532a1d267d37d8b09c05de6b46
Catalin Saveanu <catalinsaveanu@gmail.com>
2017-11-20 16:23:01 +0200

    removed serviceworker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import './index.css';
@@ -21,4 +21,4 @@ ReactDOM.render(
     </BrowserRouter>
     , document.getElementById('root')
 );
-registerServiceWorker();
+//registerServiceWorker();
